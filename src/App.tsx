9cf7d8bc05bdc477806ee715da2ece0fa2a7c491
Aleksey <alekseytim@mail.ru>
2022-10-28 14:33:44 +0300

    feat: AuthPage login and registration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { AuthPage } from "./components/AuthPage/AuthPage";
 import { Header } from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<AuthPage type={"login"} />} />
+          <Route path="/registration" element={<AuthPage type={"registration"} />} />
+          <Route path="/login" element={<AuthPage type={"login"} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
